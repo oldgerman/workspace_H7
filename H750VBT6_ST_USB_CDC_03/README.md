9b8bf70055058ed09fbf89bff81f4ae6b8c0bf5f
oldgerman <29209ddf@gmail.com>
2023-01-18 22:50:52 +0800

    完成H750VBT6_ST_USB_CDC_03

---
+++ H750VBT6_ST_USB_CDC_03/README.md
@@ -0,0 +1,211 @@
+## H750VBT6_ST_USB_CDC_03
+
+在 H750VBT6_ST_USB_CDC_02 工程的基础上，去掉中断端点和CDC类下ACM协议相关的东西，端点数量从 3个减少为2个
+
+CDC+libusb 无需中断端点，CDC+VCP无libusb的情况装载win10的VCP驱动才需要中断端点
+
+## ST培训视频
+
+### 简介
+
+[STM32 USB training - 09.3 USB CDC libusb device lab](https://www.youtube.com/watch?v=ft8LXVcYFRg&list=PLnMKNibPkDnFFRBVD206EfnnHhQZI4Hxa&index=12)
+
+> 从CubeMX的 STM32F446ZET6 + USB FS Device 模式 + VCP模式 的初始化代码进行修改
+
+此视频介绍说，CDC + libusb 是 CDC + VCP的替代品
+
+ST经常从客户那里收到关于 vcp 使用的投诉，这种类型的通信在日常应用有诸多限制
+
+- 第一个限制：VCP来自不再使用的非常旧的com端口，并且其仿真为不能即插即用的通信接口，比如运行在不同上位机的应用程序与VCP通信时，看到的可能不是同一个 COM 端口号
+
+- 第二个限制：如果使用VCP通信，会使用一个附加端点，CDC VCP协议需要3个端点，除了IN / OUT 两个端点，还有一个CMD端点，CMD端点是中断端点（附加端点），但中断端点上几乎没有通信的荷载，可以认为VCP通信浪费了一个端点（大多数STM32的USB外设的端点数量非常有限），当端点数量有限，可以将VCP放入USB复合设备
+
+因此ST推荐使用libUSB，libusb是全开源的API，可以在windows和linux平台上运行，CDC+libusb有以下优势与劣势：
+
+![](Images/ST_CDC_libusb_1.0.png)
+
+Libusb 提供了与 CDC（和其他类）设备通信的 API，无需默认的 Windows 驱动程序，它支持过时的 COM 端口模拟
+
+优势：
+
++ 在STM32设备上空闲（节省？）一个端点
++ 无需处理中断通道和额外的 VCP 层（控制信号、线路编码……）
++ 本机即插即用
+
+劣势：
+
+- 需要Windows端具有签名的驱动程序
+- 终端应用程序的发布不透明
+
+优点主要是 libusb不需要中断端点的协议，可以节约一个端点，对于CDC只需要IN/OUT两个端点，这意味着程序不需要处理中断端点、LineCoding（CDC下ACM协议的东西），在windows端的优势是没有额外的VCP协议层与本机即插即用
+
+> STM32F405的USB-FS有6个端点，1个端点0，5个双向端点
+>
+> > - CDC+VCP使用2个，剩3个双向端点
+> > - CDC+libusb使用1个，剩4个双向端点
+>
+> STM32H750的USB-FS有9个端点，1个端点0，8个双向端点
+
+缺点主要是终端应用程序和API不像标准COM端口那样通俗易懂（绝大多数程序都是准备好连接一些COM端口）
+
+### 备注
+
+视频中上位机使用 Visual Basic 的 Windows 窗体应用 (.NET Framework) 模板整的，但C# 的那一套本id不会，换QT的C++整好了
+
+视频中改 ST USB  库的 usbd_cdc.c 时，删除了 USB-LS 和 USB-HS 相关的东西，仅支持 USB FS
+
+## H750VBT6_ST_USB_CDC_02的修改
+
+### usb_desc.c
+
+为了与 02 工程的做区分，修改 PID 及其字符串
+
+> ```c
+> #define USBD_PID_FS     0x1234
+> #define USBD_PRODUCT_STRING_FS     "Keil USB Device CDC"
+> ```
+
+修改 USBD_FS_DeviceDesc[...] 的 设备类、子类，默认这两个值都是 0x02，在win10上会被装载 VCP 功能：
+
+> ```c
+>   0x02,                       /*bDeviceClass*/
+>   0x02,                       /*bDeviceSubClass*/
+> ```
+>
+> 将其都修改为0x00
+>
+> ```c
+>   0x00,                       /*bDeviceClass*/
+>   0x00,                       /*bDeviceSubClass*/
+> ```
+>
+
+### usbd_cdc.c
+
+配置描述符数组：USBD_CDC_CfgDesc[USB_CDC_CONFIG_DESC_SIZ]：
+
+> `/*Configuration Descriptor*/`
+>
+> > ```
+> > 0x02,                                       /* bNumInterfaces: 2 interfaces */
+> > ```
+> >
+> > 修改为 1 个：
+> >
+> > ```
+> > 0x01,                                       /* bNumInterfaces: 1 interfaces */
+> > ```
+>
+> 移除
+>
+> ```
+>  /* Interface Descriptor */
+> ...
+>   /* Interface descriptor type */
+> ...
+>   /* Header Functional Descriptor */
+> ...
+>   /* Call Management Functional Descriptor */
+> ...
+>   /* ACM Functional Descriptor */
+> ...
+>   /* Union Functional Descriptor */
+> ...
+>   /* Endpoint 2 Descriptor */
+> ...
+>   /*---------------------------------------------------------------------------*/
+> ```
+
+USBD_CDC_Init()
+
+> 注释掉  CDCCmdEpAdd 相关的语句：
+>
+> ```c
+>     /* Set bInterval for CDC CMD Endpoint */
+> //    pdev->ep_in[CDCCmdEpAdd & 0xFU].bInterval = CDC_HS_BINTERVAL;
+> 
+>     /* Set bInterval for CMD Endpoint */
+> //    pdev->ep_in[CDCCmdEpAdd & 0xFU].bInterval = CDC_FS_BINTERVAL;
+> 
+>   /* Open Command IN EP */
+> //  (void)USBD_LL_OpenEP(pdev, CDCCmdEpAdd, USBD_EP_TYPE_INTR, CDC_CMD_PACKET_SIZE);
+> //  pdev->ep_in[CDCCmdEpAdd & 0xFU].is_used = 1U;
+> ```
+
+USBD_CDC_DeInit()
+
+> 注释掉  CDCCmdEpAdd 相关的语句：
+>
+> ```c
+>   /* Close Command IN EP */
+> //  (void)USBD_LL_CloseEP(pdev, CDCCmdEpAdd);
+> //  pdev->ep_in[CDCCmdEpAdd & 0xFU].is_used = 0U;
+> //  pdev->ep_in[CDCCmdEpAdd & 0xFU].bInterval = 0U;
+> ```
+
+### usbd_cdc.h
+
+由于删除很多配置描述符数组的元素，因此数组大小也要减小：
+
+```c
+#define USB_CDC_CONFIG_DESC_SIZ                     32U /* 67U 减少为 32U*/
+```
+
+此值得自己数出来，因为既作为配置描述符数组初始化时的大小，也作为其内部的元素值
+
+![](Images/配置描述符大小：USB_CDC_CONFIG_DESC_SIZ宏.png)
+
+### usbd_cdc_if.c
+
+去掉 获取 linecoding 内的代码，linecoding是ACM协议下的东西，不需要了
+
+```c
+    case CDC_GET_LINE_CODING:
+
+    break;
+```
+
+## Zadig 覆盖驱动
+
+| ![Zadig安装libusb-win32驱动1](Images/Zadig安装libusb-win32驱动1.png) | ![Zadig安装libusb-win32驱动2](Images/Zadig安装libusb-win32驱动2.png) |
+| ------------------------------------------------------------ | ------------------------------------------------------------ |
+
+## QT上位机的修改
+
+在 H750VBT6_ST_USB_CDC_02 目录下的 USB_QT 工程的基础上修改
+
+### mainwindow.cpp
+
+m_dev_PRODUCT：
+
+```c
+#define m_dev_PRODUCT   0x1234	 /* Product ID of the m_dev        */
+```
+
+MainWindow::usbdataupdate()：端点地址改为固定的 0x81
+
+```c
+        ret = usb_bulk_read(m_dev_handle,
+                            0x81,   //endpoint IN 地址 0x81
+                            RecTraData,
+                            2048,
+                            128);
+```
+
+## 测试
+
+### 硬汉哥emWin上位机抓取的USB设备信息
+
+相比 H750VBT6_ST_USB_CDC_02 ，H750VBT6_ST_USB_CDC_03 的改动使得描述符的蓝色箭头的信息被修改
+
+备注：需要临时将 PID 改回 0x3505
+
+| H750VBT6_ST_USB_CDC_02                                       | H750VBT6_ST_USB_CDC_03                                       |
+| ------------------------------------------------------------ | ------------------------------------------------------------ |
+| ![硬汉哥emWin上位机抓取的USB设备信息](../H750VBT6_ST_USB_CDC_02/Images/硬汉哥emWin上位机抓取的USB设备信息.png) | ![emWin上位机获取的描述符变化](Images/emWin上位机获取的描述符变化.png) |
+
+### 修改后的硬汉哥QT USB BULK上位机通信
+
+和 [STM32 USB training - 09.3 USB CDC libusb device lab](https://www.youtube.com/watch?v=ft8LXVcYFRg&list=PLnMKNibPkDnFFRBVD206EfnnHhQZI4Hxa&index=12) 说的一样，去掉 中断端点 对CDC BULK 无影响
+
+![Demo_QT上位机](Images/Demo_QT上位机.png)
---
+++ H750VBT6_ST_USB_CDC_03/USB_QT/USB/mainwindow.cpp
@@ -0,0 +1,318 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "lusb0_usb.h"
+
+#include <QtCharts/QChartView>
+#include <QtCharts/QLineSeries>
+#include <QtCharts/QValueAxis>
+#include <QtCharts/QCategoryAxis>
+
+//#pragma warning (disable:4819)
+
+#define m_dev_VENDOR    0xC251	 /* Vendor ID of the m_dev         */
+#define m_dev_PRODUCT   0x1234	 /* Product ID of the m_dev        */
+#define m_dev_DATA_SIZE 10240    /* Number of bytes to get at once */
+
+char RecTraData[m_dev_DATA_SIZE] = {0};
+struct usb_bus *bus;
+struct usb_device * m_dev = NULL;
+struct usb_dev_handle *m_dev_handle;
+
+int OpenUSB(void)
+{
+    int ret;
+
+    QString str;
+
+    /* 第1步：USB 初始化 */
+    usb_init();
+
+    /* 第2步：寻找系统上的usb总线，任何usb设备都通过usb总线和计算机总线通信。进而和其他设备通信。此函数返回总线数 */
+    ret = usb_find_busses();
+    if(ret < 0)
+    {
+
+    }
+
+    /* 第3步：寻找总线上的usb设备，这个函数必要在调用usb_find_busses()后使用。以上的三个函数都是一开始就要用到的，此函数返回设备数量 */
+    ret = usb_find_devices();
+    if(ret < 0)
+    {
+        str = str.asprintf("寻找USB设备失败：%d", ret);
+        qDebug() << str;
+        return -1;
+    }
+    else
+    {
+        /* 成功 */
+        str = str.asprintf("寻找USB设备成功：%d", ret);
+        qDebug() << str;
+    }
+
+    /*  第4步：这个函数返回总线的列表，在高一些的版本中已经用不到了 */
+    for(bus = usb_get_busses(); bus; bus = bus->next)
+    {
+        struct usb_device *dev;
+        for(dev = bus->devices; dev; dev = dev->next)
+        {
+            if(dev->descriptor.idVendor == m_dev_VENDOR && dev->descriptor.idProduct == m_dev_PRODUCT)
+            m_dev=dev;
+        }
+    }
+    if(!m_dev)
+    {  
+        str = str.asprintf("设备查找失败：%d", -2);
+        qDebug() << str;
+        return -2;
+    }
+
+    /*  第5步：
+        打开要使用的设备，在对硬件进行操作前必须要调用usb_open 来打开设备，这里大家看到有两个结构体
+        usb_dev_handle 和 usb_device 是我们在开发中经常碰到的，有必要把它们的结构看一看。在libusb 中
+        的usb.h和usbi.h中有定义。
+
+        这里我们不妨理解为返回的 usb_dev_handle 指针是指向设备的句柄，而行参里输入就是需要打开的设备。
+    */
+    m_dev_handle = usb_open(m_dev);
+    if(!m_dev_handle)
+    {
+        str = str.asprintf("设备打开失败：%d", -3);
+        qDebug() << str;
+        return -3;
+    }
+
+    /*  第6步：
+        设置当前设备使用的configuration，参数configuration 是你要使用的configurtation descriptoes中的
+        bConfigurationValue, 返回0成功，<0失败( 一个设备可能包含多个configuration,比如同时支持高速和
+        低速的设备就有对应的两个configuration,详细可查看usb标准)
+
+    */
+    if(usb_set_configuration(m_dev_handle, 1) < 0)
+    {
+         /* 失败 未处理 */
+    }
+
+    /*
+        第7步：
+        注册与操作系统通信的接口，这个函数必须被调用，因为只有注册接口，才能做相应的操作。
+
+        Interface 指 bInterfaceNumber. (usb_release_interface 与之相对应，也是必须调用的函数)
+    */
+    if(usb_claim_interface(m_dev_handle, 1) < 0) //claim_interface 0指向第一个设备
+    {
+         /* 失败 未处理 */
+    }
+
+    return 1;
+}
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{ 
+    ui->setupUi(this);
+
+    /* ====================================  */
+    QChart *chart = new QChart();
+    chart->legend()->hide();
+    chart->setTitle("Simple line chart example");
+
+    /* ====================================  */
+    QValueAxis *axisX = new QValueAxis;
+    axisX->setTickCount(10);
+    axisX->setMin(0);
+    axisX->setMax(10);
+    chart->addAxis(axisX, Qt::AlignBottom);
+
+    /* ====================================  */
+    series = new QLineSeries();
+    chart->addSeries(series);
+
+    /* ====================================  */
+    QValueAxis *axisY = new QValueAxis;
+    axisY->setLinePenColor(series->pen().color());
+    axisY->setMin(0);
+    axisY->setMax(100);
+
+    chart->addAxis(axisY, Qt::AlignLeft);
+    series->attachAxis(axisX);
+    series->attachAxis(axisY);
+
+    QChartView *chartView = new QChartView(chart);
+    chartView->setRenderHint(QPainter::Antialiasing);
+    chartView->setGeometry(QRect(10, 360, 791, 261));
+    chartView->setParent(this);
+
+    /* 创建定时器 */
+    timer = new QTimer(this);
+
+    /* 关联槽函数 */
+    connect(timer, SIGNAL(timeout()), this, SLOT(usbdataupdate()));
+
+    /* 启动定时器，100ms一次 */
+    timer->start(100);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    QString debugstr;
+    int usbstatus;
+    char str[64];
+
+    if (UsbOpenFlag == 0)
+    {
+        /* 打开USB连接 */
+        usbstatus = OpenUSB();
+
+        if (usbstatus == -1)
+        {
+            ui->label->setText("USB开关状态：寻找USB设备失败");
+        }
+        else if (usbstatus == -2)
+        {
+            ui->label->setText("USB开关状态：设备查找失败，VID和PID不匹配");
+        }
+        else if (usbstatus == -3)
+        {
+            ui->label->setText("USB开关状态：设备打开失败");
+        }
+        else
+        {
+            /* 打开成功 */
+            UsbOpenFlag = 1;
+
+            ui->label->setText("USB开关状态：设备打开成功");
+
+            ui->pushButton->setText("关闭USB设备");
+
+            debugstr = debugstr.asprintf("设备名：%s", m_dev->filename);
+            ui->label_2->setText(debugstr);
+
+            usb_get_string_simple(m_dev_handle, m_dev->descriptor.iManufacturer, str, sizeof(str));
+            debugstr = debugstr.asprintf("厂商：%s", str);
+
+            ui->label_3->setText(debugstr);
+
+            usb_get_string_simple(m_dev_handle, m_dev->descriptor.iProduct, str, sizeof(str));
+            debugstr = debugstr.asprintf("产品：%s", str);
+            ui->label_4->setText(debugstr);
+
+            usb_get_string_simple(m_dev_handle, m_dev->descriptor.iSerialNumber, str, sizeof(str));
+            debugstr = debugstr.asprintf("序列号：%s",str);
+
+            ui->label_5->setText(debugstr);
+        }
+    }
+    else
+    {
+        UsbOpenFlag = 0;
+        DataSampleStart = 0;
+        usb_release_interface(m_dev_handle, 1);
+        //usb_reset(m_dev_handle);
+       // usb_close(m_dev_handle);
+
+        ui->label->setText("USB开关状态：关闭");
+        ui->pushButton->setText("打开USB设备");
+        ui->pushButton_2->setText("开启数据采集");
+    }
+
+    qDebug()<<str;
+}
+
+void MainWindow::usbdataupdate()
+{
+    int ret;
+    QString debugstr;
+
+    static uint32_t timeStamp = 0; //时间戳
+    qDebug("timer：%lums", timeStamp);
+    timeStamp += 100;   //定时器，100ms一次
+
+    if ((UsbOpenFlag == 1) && (DataSampleStart == 1))
+    {
+        /*
+           H750VBT6_ST_USB_CDC_02工程的 usbd_cdc.h 中断端点的地址 CDC_CMD_EP 是 0x82U， 0x81和0x01作为BULK端点IN/OUT地址
+           因为 m_dev->config->interface->altsetting->endpoint->bEndpointAddress值为0x82，所以需要减1才是BULK端点IN地址
+           注意：硬汉哥的教程中RL-USB的配置是STM32F429BIT6的 0x81 作为 中断端点地址, 0x82和0x02作为BULK端点IN/OUT地址
+        */
+        ret = usb_bulk_read(m_dev_handle,
+                            0x81,   //endpoint IN 地址 0x81
+                            RecTraData,
+                            2048,
+                            128);
+
+        qDebug("bEndpointAddress: 0x%02X", m_dev->config->interface->altsetting->endpoint->bEndpointAddress);   //打印OUT端点地址
+        /* 显示返回的数据个数 */
+        debugstr = debugstr.asprintf("返回字节数：%d", ret);
+
+        ui->label_7->setText(debugstr);
+
+        /* 显示前25个数据 */
+        debugstr = debugstr.asprintf("返回部分数据：%02X %02X %02X %02X %02X ",
+                      RecTraData[0], RecTraData[1], RecTraData[2], RecTraData[3], RecTraData[4]);
+
+        for (int i = 5; i < 25; i+=5)
+        {
+            debugstr += debugstr.asprintf("%02X %02X %02X %02X %02X ",
+                  RecTraData[i], RecTraData[i+1], RecTraData[i+2], RecTraData[i+3], RecTraData[i+4]);
+        }
+
+        ui->label_8->setText(debugstr);
+
+        series->clear();
+        for(int i = 0; i < 10; i++)
+        {
+            *series << QPointF(i, RecTraData[i]);
+        }
+
+        /* fibre框架处理USB接收的消息要求至少末尾要有回车符 */
+        RecTraData[1] = '\r';
+        RecTraData[2] = '\n';
+
+        if(DataSampleStart == 1)
+        {
+            RecTraData[0] = 0x02;
+            /* 执行发送 */
+            ret = usb_bulk_write(m_dev_handle,
+                                 1,                 //endpoint OUT 地址 0x01
+                                 RecTraData,
+                                 3,                 //以 一个数字+"\r\n" 的格式发送命令，共3个字符
+                                 128);
+            debugstr = debugstr.asprintf("发送命令：%02X", RecTraData[0]);
+        }
+
+
+        ui->label_6->setText(debugstr);
+    }
+
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    if (UsbOpenFlag == 1)
+    {
+        /* 启动数据采集*/
+        if (DataSampleStart == 0)
+        {
+            DataSampleStart = 1;
+            ui->pushButton_2->setText("停止数据采集");
+
+        }
+        /* 停止数据采集 */
+        else
+        {
+            DataSampleStart = 0;
+            ui->pushButton_2->setText("开启数据采集");
+        }
+    }
+}
+
+void MainWindow::on_groupBox_clicked()
+{
+
+}
