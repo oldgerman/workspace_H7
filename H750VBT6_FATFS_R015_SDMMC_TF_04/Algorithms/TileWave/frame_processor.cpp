74658a5f0762e47c9e00494de39a5303f783b344
oldgerman <29209ddf@gmail.com>
2023-04-01 02:26:05 +0800

    2023-04-01_02:26

--- H750VBT6_FATFS_R015_SDMMC_TF_04/Algorithms/TileWave/frame_processor.cpp
+++ H750VBT6_FATFS_R015_SDMMC_TF_04/Algorithms/TileWave/frame_processor.cpp
@@ -114,100 +114,102 @@ static void frameProcessorTask(void* argument)
 		}
 
 		for(uint32_t i = 0; i < buffer_num / 2; i++) {
-			frame[i].ctrl_f32 = arm_sin_f32(2.0f * user_pi * i / (buffer_num / 2));
+			frame[i].ctrl_f32 = arm_sin_f32(2.0f * user_pi * i / (buffer_num / 2)) * 25;
 		}
 
-		if(firstInitExistingWaveFile)
+		if(frame_writeLayerBuffer == 0)
 		{
-			if(frame_writeLayerBuffer == 0)
-			{
-				/* 停止前需要做最后一次 52KB 特殊切片 */
-				msg.type = TileWave::EVENT_LAST_WRITE_LAYER_BUFFER;
-				if(firstSliceStop) {
-					xSliceState = SLICE_LAST_WRITE_BUFFER;
-				}
+			/* 停止前需要做最后一次 52KB 特殊切片 */
+			msg.type = TileWave::EVENT_LAST_WRITE_LAYER_BUFFER;
+			if(firstSliceStop) {
+				xSliceState = SLICE_LAST_WRITE_BUFFER;
 			}
-			else
-			{
-				msg.type = TileWave::EVENT_WRITE_LAYER_BUFFER;
-				xSliceState = SLICE_WRITE_BUFFER;
-				firstSliceStop = 1;
-			}
+		}
+		else
+		{
+			msg.type = TileWave::EVENT_WRITE_LAYER_BUFFER;
+			xSliceState = SLICE_WRITE_BUFFER;
+			firstSliceStop = 1;
+		}
 
-			if(xSliceState == SLICE_WRITE_BUFFER || xSliceState == SLICE_LAST_WRITE_BUFFER)
-			{
-				/* 若为最后一次写，则下次停止写 */
-				if(xSliceState == SLICE_LAST_WRITE_BUFFER) {
-					xSliceState = SLICE_USER_STOP;
-					firstSliceStop = 0;
-				}
+		if(firstInitExistingWaveFile) {
+			firstInitExistingWaveFile = 0;
+			xSliceState = SLICE_USER_STOP;
+		}
 
-				/* 用作数据测试 */
-	//				memset(frame, '.', sizeof(frame)); 									// 帧缓冲区全部归'.'
-	//				sprintf((char*)&(frame[0].ctrl_u8[0]), "%4ld", xTileWave.ulPeriod); // 第一个元素写入当前周期数
-
-				/* 切片瓦片缓冲区暂存到层缓冲区 */
-				msg.xWriteLayerBufferParam = xTileWave.sliceTileBuffer(frame[0].ctrl_u8, msg.type);
-
-				/* 向消息队列写消息，通知写事件 */
-				osStatus = osMessageQueuePut(
-						xTileWave.xMsgQueue,
-						&msg, 	// 指向消息的指针，会使用 memcpy 拷贝消息地址上的数据，不是直接传递地址
-						0U, 	// 消息优先级 0
-						0U);	// 写阻塞时间
-
-				/** 若消息队列存消息失败
-				  * 则保存本次写入参数的地址和大小信息，但丢弃数据 */
-				if(osStatus != osOK) {
-					/* 检查文件头对象的脏数据表的剩余大小 */
-
-					/* TODO 保存数据, 一次8byte */
-	//				msg.xWriteLayerBufferParam.ulAddr;
-	//				msg.xWriteLayerBufferParam.ulSize;
-				}
-
-				/* 记录消息队列中消息个数 */
-				ulQueueCount = osMessageQueueGetCount(xTileWave.xMsgQueue);
-				if(ulQueueCountHistoryMax < ulQueueCount) {
-					ulQueueCountHistoryMax = ulQueueCount;
-				}
-
-				/* 记录内存池剩余大小 */
-				ulMemoryMin = DRAM_SRAM1.getMemFree();
-				if(ulHistoryMemoryMin > ulMemoryMin) {
-					ulHistoryMemoryMin = ulMemoryMin;
-				}
-
-				/* 输出本次任务的信息 */
-				printf("|  frameTask  | osStatus = %1d | queue count = %1ld | queue count hisrotry max = %1ld | history free min = %6ld | \r\n" ,
-						osStatus, ulQueueCount, ulQueueCountHistoryMax, ulHistoryMemoryMin);
-
-				ulPeriodCount++;	// 更新周期计数器
+		if(xSliceState == SLICE_WRITE_BUFFER || xSliceState == SLICE_LAST_WRITE_BUFFER)
+		{
+			/* 若为最后一次写，则下次停止写 */
+			if(xSliceState == SLICE_LAST_WRITE_BUFFER) {
+				xSliceState = SLICE_USER_STOP;
+				firstSliceStop = 0;
 			}
 
-			/* 4096次后停止，TODO: 写满后从首地址覆盖写入 */
-			if(ulPeriodCount == xTileWave.ulLayerTileNumMax - 1) {
-				frame_writeLayerBuffer = 0;
-				xSliceState = SLICE_SELF_STOP;
+			/* 用作数据测试 */
+//				memset(frame, '.', sizeof(frame)); 									// 帧缓冲区全部归'.'
+//				sprintf((char*)&(frame[0].ctrl_u8[0]), "%4ld", xTileWave.ulPeriod); // 第一个元素写入当前周期数
+
+			/* 切片瓦片缓冲区暂存到层缓冲区 */
+			msg.xWriteLayerBufferParam = xTileWave.sliceTileBuffer(frame[0].ctrl_u8, msg.type);
+
+			/* 向消息队列写消息，通知写事件 */
+			osStatus = osMessageQueuePut(
+					xTileWave.xMsgQueue,
+					&msg, 	// 指向消息的指针，会使用 memcpy 拷贝消息地址上的数据，不是直接传递地址
+					0U, 	// 消息优先级 0
+					0U);	// 写阻塞时间
+
+			/** 若消息队列存消息失败
+			  * 则保存本次写入参数的地址和大小信息，但丢弃数据 */
+			if(osStatus != osOK) {
+				/* 检查文件头对象的脏数据表的剩余大小 */
+
+				/* TODO 保存数据, 一次8byte */
+//				msg.xWriteLayerBufferParam.ulAddr;
+//				msg.xWriteLayerBufferParam.ulSize;
 			}
 
-			if(frame_writeLayerBuffer == 0
-					&& (xSliceState != SLICE_SELF_STOP))
-			{
-				/* 切片前复位一些变量 */
-				xTileWave.resetVariablesBeforeSlice();
-				ulMemoryMin = DRAM_SRAM1.getMemFree();
+			/* 记录消息队列中消息个数 */
+			ulQueueCount = osMessageQueueGetCount(xTileWave.xMsgQueue);
+			if(ulQueueCountHistoryMax < ulQueueCount) {
+				ulQueueCountHistoryMax = ulQueueCount;
+			}
+
+			/* 记录内存池剩余大小 */
+			ulMemoryMin = DRAM_SRAM1.getMemFree();
+			if(ulHistoryMemoryMin > ulMemoryMin) {
 				ulHistoryMemoryMin = ulMemoryMin;
-				ulPeriodCount = 0;
-				ulQueueCountHistoryMax = 0;
 			}
 
-			if(frame_readLayerBuffer) {
-				frame_readLayerBuffer = 0;
-				msg.type = TileWave::EVENT_READ_LAYER_BUFFER;
-				msg.xReadLayerBufferParam = xReadLayerBufferParam;
-				osStatus = osMessageQueuePut(xTileWave.xMsgQueue, &msg, 0U, 0U);
-			}
+			/* 输出本次任务的信息 */
+			printf("|  frameTask  | osStatus = %1d | queue count = %1ld | queue count hisrotry max = %1ld | history free min = %6ld | \r\n" ,
+					osStatus, ulQueueCount, ulQueueCountHistoryMax, ulHistoryMemoryMin);
+
+			ulPeriodCount++;	// 更新周期计数器
+		}
+
+		/* 4096次后停止，TODO: 写满后从首地址覆盖写入 */
+		if(ulPeriodCount == xTileWave.ulLayerTileNumMax - 1) {
+			frame_writeLayerBuffer = 0;
+			xSliceState = SLICE_SELF_STOP;
+		}
+
+		if(frame_writeLayerBuffer == 0
+				&& (xSliceState != SLICE_SELF_STOP))
+		{
+			/* 切片前复位一些变量 */
+			xTileWave.resetVariablesBeforeSlice();
+			ulMemoryMin = DRAM_SRAM1.getMemFree();
+			ulHistoryMemoryMin = ulMemoryMin;
+			ulPeriodCount = 0;
+			ulQueueCountHistoryMax = 0;
+		}
+
+		if(frame_readLayerBuffer) {
+			frame_readLayerBuffer = 0;
+			msg.type = TileWave::EVENT_READ_LAYER_BUFFER;
+			msg.xReadLayerBufferParam = xReadLayerBufferParam;
+			osStatus = osMessageQueuePut(xTileWave.xMsgQueue, &msg, 0U, 0U);
 		}
 
 		xTaskPeriod = 1000 / frame_freq;	/* 调度周期，单位ms */
