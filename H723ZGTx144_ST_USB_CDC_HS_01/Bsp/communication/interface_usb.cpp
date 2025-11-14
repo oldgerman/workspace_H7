36684edef61bc7b4733b85361c958ca6e699766a
pengshiao <2920956602@qq.com>
2025-11-14 20:56:55 +0800

    2025-11-14_20:27_添加H723高速USB串口高级例程:FreeRTOS+Fibre框架

---
+++ H723ZGTx144_ST_USB_CDC_HS_01/Bsp/communication/interface_usb.cpp
@@ -0,0 +1,162 @@
+#include "common_inc.h"
+#include "ascii_processor.hpp"
+#include "usbd_cdc.h"
+#include "usbd_cdc_if.h"
+#include "usb_device.h"
+#include "interface_usb.hpp"
+
+osThreadId_t usbServerTaskHandle;
+USBStats_t usb_stats_ = {0};
+
+class USBSender : public PacketSink
+{
+public:
+    USBSender(const osSemaphoreId &sem_usb_tx)
+        : sem_usb_tx_(sem_usb_tx)
+    {}
+
+    int process_packet(uint8_t *buffer, size_t length) override
+    {
+        // cannot send partial packets
+        if (length > CDC_DATA_FS_MAX_PACKET_SIZE)
+            return -1;
+        // wait for USB interface to become ready
+        if (osSemaphoreAcquire(sem_usb_tx_, PROTOCOL_SERVER_TIMEOUT_MS) != osOK)
+        {
+            // If the host resets the device it might be that the TX-complete handler is never called
+            // and the sem_usb_tx_ semaphore is never released. To handle this we just override the
+            // TX buffer if this wait times out. The implication is that the channel is no longer lossless.
+            // TODO: handle endpoint reset properly
+            usb_stats_.tx_overrun_cnt++;
+        }
+
+        // transmit packet
+        uint8_t status = CDC_Transmit_HS(const_cast<uint8_t *>(buffer), length);
+        if (status != USBD_OK)
+        {
+            osSemaphoreRelease(sem_usb_tx_);
+            return -1;
+        }
+        usb_stats_.tx_cnt++;
+
+        return 0;
+    }
+
+private:
+    const osSemaphoreId &sem_usb_tx_;
+};
+
+// Note we could have independent semaphores here to allow concurrent transmission
+USBSender usb_packet_output_cdc(sem_usb_tx);
+
+class TreatPacketSinkAsStreamSink : public StreamSink
+{
+public:
+    TreatPacketSinkAsStreamSink(PacketSink &output) : output_(output)
+    {}
+
+    int process_bytes(uint8_t *buffer, size_t length, size_t *processed_bytes)
+    {
+        // Loop to ensure all bytes get sent
+        while (length)
+        {
+            size_t chunk = length < CDC_DATA_FS_MAX_PACKET_SIZE ? length : CDC_DATA_FS_MAX_PACKET_SIZE;
+            if (output_.process_packet(buffer, length) != 0)
+                return -1;
+            buffer += chunk;
+            length -= chunk;
+            if (processed_bytes)
+                *processed_bytes += chunk;
+        }
+        return 0;
+    }
+
+    size_t get_free_space()
+    { return SIZE_MAX; }
+
+private:
+    PacketSink &output_;
+
+/**
+ * 构造 usb_stream_output 对象时构造函数将 usb_packet_output_cdc 对象传给引用类型成员 output_ ，
+ * 那么成员函数 process_bytes() 调用的 output_.process_packet ，是 usb_packet_output_cdc 对象的虚函数
+ */
+} usb_stream_output(usb_packet_output_cdc);
+
+
+// This is used by the printf feature. Hence the above statics, and below seemingly random ptr (it's externed)
+// TODO: less spaghetti code
+/**
+ * 指向虚基类 StreamSink 的指针指向派生类 TreatPacketSinkAsStreamSink 对象 usb_stream_output
+ */
+StreamSink *usb_stream_output_ptr = &usb_stream_output;
+
+struct USBInterface
+{
+    uint8_t *rx_buf = nullptr;
+    uint32_t rx_len = 0;
+    bool data_pending = false;
+    uint8_t out_ep;
+    uint8_t in_ep;
+    USBSender &usb_sender;
+};
+
+// Note: statics make this less modular.
+// Note: we use a single rx semaphore and loop over data_pending to allow a single pump loop thread
+static USBInterface CDC_interface = {
+    .rx_buf = nullptr,
+    .rx_len = 0,
+    .data_pending = false,
+    .out_ep = CDC_OUT_EP,
+    .in_ep = CDC_IN_EP,
+    .usb_sender = usb_packet_output_cdc,
+};
+
+static void UsbServerTask(void *ctx)
+{
+    (void) ctx;
+
+    for (;;)
+    {
+        // const uint32_t usb_check_timeout = 1; // ms
+        osStatus sem_stat = osSemaphoreAcquire(sem_usb_rx, osWaitForever);
+        if (sem_stat == osOK)
+        {
+            usb_stats_.rx_cnt++;
+
+            // CDC Interface
+            if (CDC_interface.data_pending)
+            {
+                CDC_interface.data_pending = false;
+
+                ASCII_protocol_parse_stream(CDC_interface.rx_buf, CDC_interface.rx_len, usb_stream_output);
+                USBD_CDC_ReceivePacket(&hUsbDeviceHS);  // Allow next packet
+            }
+        }
+    }
+}
+
+// Called from CDC_Receive_FS callback function, this allows the communication
+// thread to handle the incoming data
+void usb_rx_process_packet(uint8_t *buf, uint32_t len)
+{
+    // We don't allow the next USB packet until the previous one has been processed completely.
+    // Therefore it's safe to write to these vars directly since we know previous processing is complete.
+	CDC_interface.rx_buf = buf;
+	CDC_interface.rx_len = len;
+	CDC_interface.data_pending = true;
+    osSemaphoreRelease(sem_usb_rx);
+}
+
+const uint32_t usbServerTaskStackSize = 512 * 4;
+const osThreadAttr_t usbServerTask_attributes = {
+    .name = "UsbServerTask",
+    .stack_size = usbServerTaskStackSize,
+    .priority = (osPriority_t) osPriorityAboveNormal,
+};
+
+void StartUsbServer()
+{
+    // Start USB communication thread
+    usbServerTaskHandle = osThreadNew(UsbServerTask, nullptr, &usbServerTask_attributes);
+}
---
+++ H723ZGTx144_ST_USB_CDC_HS_01/USB_DEVICE/App/usbd_cdc_if.c
@@ -0,0 +1,446 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : usbd_cdc_if.c
+  * @version        : v1.0_Cube
+  * @brief          : Usb device for Virtual Com Port.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "usbd_cdc_if.h"
+
+/* USER CODE BEGIN INCLUDE */
+#include <cmsis_os.h>
+#include "interface_usb.hpp"
+/* USER CODE END INCLUDE */
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE END PV */
+
+/** @addtogroup STM32_USB_OTG_DEVICE_LIBRARY
+  * @brief Usb device library.
+  * @{
+  */
+
+/** @addtogroup USBD_CDC_IF
+  * @{
+  */
+
+/** @defgroup USBD_CDC_IF_Private_TypesDefinitions USBD_CDC_IF_Private_TypesDefinitions
+  * @brief Private types.
+  * @{
+  */
+
+/* USER CODE BEGIN PRIVATE_TYPES */
+
+/* USER CODE END PRIVATE_TYPES */
+
+/**
+  * @}
+  */
+
+/** @defgroup USBD_CDC_IF_Private_Defines USBD_CDC_IF_Private_Defines
+  * @brief Private defines.
+  * @{
+  */
+
+/* USER CODE BEGIN PRIVATE_DEFINES */
+/* USER CODE END PRIVATE_DEFINES */
+
+/**
+  * @}
+  */
+
+/** @defgroup USBD_CDC_IF_Private_Macros USBD_CDC_IF_Private_Macros
+  * @brief Private macros.
+  * @{
+  */
+
+/* USER CODE BEGIN PRIVATE_MACRO */
+
+/* USER CODE END PRIVATE_MACRO */
+
+/**
+  * @}
+  */
+
+/** @defgroup USBD_CDC_IF_Private_Variables USBD_CDC_IF_Private_Variables
+  * @brief Private variables.
+  * @{
+  */
+
+/* Create buffer for reception and transmission           */
+/* It's up to user to redefine and/or remove those define */
+/** Received data over USB are stored in this buffer      */
+uint8_t UserRxBufferHS[APP_RX_DATA_SIZE];
+
+/** Data to send over USB CDC are stored in this buffer   */
+uint8_t UserTxBufferHS[APP_TX_DATA_SIZE];
+
+/* USER CODE BEGIN PRIVATE_VARIABLES */
+
+/* USER CODE END PRIVATE_VARIABLES */
+
+/**
+  * @}
+  */
+
+/** @defgroup USBD_CDC_IF_Exported_Variables USBD_CDC_IF_Exported_Variables
+  * @brief Public variables.
+  * @{
+  */
+
+extern USBD_HandleTypeDef hUsbDeviceHS;
+
+/* USER CODE BEGIN EXPORTED_VARIABLES */
+
+/* USER CODE END EXPORTED_VARIABLES */
+
+/**
+  * @}
+  */
+
+/** @defgroup USBD_CDC_IF_Private_FunctionPrototypes USBD_CDC_IF_Private_FunctionPrototypes
+  * @brief Private functions declaration.
+  * @{
+  */
+
+static int8_t CDC_Init_HS(void);
+static int8_t CDC_DeInit_HS(void);
+static int8_t CDC_Control_HS(uint8_t cmd, uint8_t* pbuf, uint16_t length);
+static int8_t CDC_Receive_HS(uint8_t* pbuf, uint32_t *Len);
+static int8_t CDC_TransmitCplt_HS(uint8_t *pbuf, uint32_t *Len, uint8_t epnum);
+
+/* USER CODE BEGIN PRIVATE_FUNCTIONS_DECLARATION */
+
+/* USER CODE END PRIVATE_FUNCTIONS_DECLARATION */
+
+/**
+  * @}
+  */
+
+USBD_CDC_ItfTypeDef USBD_Interface_fops_HS =
+{
+  CDC_Init_HS,
+  CDC_DeInit_HS,
+  CDC_Control_HS,
+  CDC_Receive_HS,
+  CDC_TransmitCplt_HS
+};
+
+/* Private functions ---------------------------------------------------------*/
+
+/**
+  * @brief  Initializes the CDC media low layer over the USB HS IP
+  * @retval USBD_OK if all operations are OK else USBD_FAIL
+  */
+static int8_t CDC_Init_HS(void)
+{
+  /* USER CODE BEGIN 8 */
+  /* Set Application Buffers */
+  USBD_CDC_SetTxBuffer(&hUsbDeviceHS, UserTxBufferHS, 0);
+  USBD_CDC_SetRxBuffer(&hUsbDeviceHS, UserRxBufferHS);
+  return (USBD_OK);
+  /* USER CODE END 8 */
+}
+
+/**
+  * @brief  DeInitializes the CDC media low layer
+  * @param  None
+  * @retval USBD_OK if all operations are OK else USBD_FAIL
+  */
+static int8_t CDC_DeInit_HS(void)
+{
+  /* USER CODE BEGIN 9 */
+  return (USBD_OK);
+  /* USER CODE END 9 */
+}
+
+/**
+  * @brief  Manage the CDC class requests
+  * @param  cmd: Command code
+  * @param  pbuf: Buffer containing command data (request parameters)
+  * @param  length: Number of data to be sent (in bytes)
+  * @retval Result of the operation: USBD_OK if all operations are OK else USBD_FAIL
+  */
+static int8_t CDC_Control_HS(uint8_t cmd, uint8_t* pbuf, uint16_t length)
+{
+  /* USER CODE BEGIN 10 */
+  switch(cmd)
+  {
+  case CDC_SEND_ENCAPSULATED_COMMAND:
+
+    break;
+
+  case CDC_GET_ENCAPSULATED_RESPONSE:
+
+    break;
+
+  case CDC_SET_COMM_FEATURE:
+
+    break;
+
+  case CDC_GET_COMM_FEATURE:
+
+    break;
+
+  case CDC_CLEAR_COMM_FEATURE:
+
+    break;
+
+  /*******************************************************************************/
+  /* Line Coding Structure                                                       */
+  /*-----------------------------------------------------------------------------*/
+  /* Offset | Field       | Size | Value  | Description                          */
+  /* 0      | dwDTERate   |   4  | Number |Data terminal rate, in bits per second*/
+  /* 4      | bCharFormat |   1  | Number | Stop bits                            */
+  /*                                        0 - 1 Stop bit                       */
+  /*                                        1 - 1.5 Stop bits                    */
+  /*                                        2 - 2 Stop bits                      */
+  /* 5      | bParityType |  1   | Number | Parity                               */
+  /*                                        0 - None                             */
+  /*                                        1 - Odd                              */
+  /*                                        2 - Even                             */
+  /*                                        3 - Mark                             */
+  /*                                        4 - Space                            */
+  /* 6      | bDataBits  |   1   | Number Data bits (5, 6, 7, 8 or 16).          */
+  /*******************************************************************************/
+  case CDC_SET_LINE_CODING:
+
+    break;
+
+  case CDC_GET_LINE_CODING:
+      pbuf[0] = (uint8_t)(115200);
+      pbuf[1] = (uint8_t)(115200 >> 8);
+      pbuf[2] = (uint8_t)(115200 >> 16);
+      pbuf[3] = (uint8_t)(115200 >> 24);
+      pbuf[4] = 0;  // stop bits (1)
+      pbuf[5] = 0;  // parity (none)
+      pbuf[6] = 8;  // number of bits (8)
+    break;
+
+  case CDC_SET_CONTROL_LINE_STATE:
+
+    break;
+
+  case CDC_SEND_BREAK:
+
+    break;
+
+  default:
+    break;
+  }
+
+  return (USBD_OK);
+  /* USER CODE END 10 */
+}
+
+/**
+  * @brief Data received over USB OUT endpoint are sent over CDC interface
+  *         through this function.
+  *
+  *         @note
+  *         This function will issue a NAK packet on any OUT packet received on
+  *         USB endpoint until exiting this function. If you exit this function
+  *         before transfer is complete on CDC interface (ie. using DMA controller)
+  *         it will result in receiving more data while previous ones are still
+  *         not sent.
+  *
+  * @param  Buf: Buffer of data to be received
+  * @param  Len: Number of data received (in bytes)
+  * @retval Result of the operation: USBD_OK if all operations are OK else USBD_FAILL
+  */
+static int8_t CDC_Receive_HS(uint8_t* Buf, uint32_t *Len)
+{
+  /* USER CODE BEGIN 11 */
+#if 0   // CubeMX Generated
+  USBD_CDC_SetRxBuffer(&hUsbDeviceHS, &Buf[0]);
+  USBD_CDC_ReceivePacket(&hUsbDeviceHS);
+#else
+  usb_rx_process_packet(Buf, *Len);
+#endif
+  return (USBD_OK);
+  /* USER CODE END 11 */
+}
+
+/**
+  * @brief  Data to send over USB IN endpoint are sent over CDC interface
+  *         through this function.
+  * @param  Buf: Buffer of data to be sent
+  * @param  Len: Number of data to be sent (in bytes)
+  * @retval Result of the operation: USBD_OK if all operations are OK else USBD_FAIL or USBD_BUSY
+  */
+uint8_t CDC_Transmit_HS(uint8_t* Buf, uint16_t Len)
+{
+  uint8_t result = USBD_OK;
+  /* USER CODE BEGIN 12 */
+#if 0 //CubeMX Generated
+  /* Code automatically generated by CubeMX */
+  USBD_CDC_HandleTypeDef *hcdc = (USBD_CDC_HandleTypeDef*)hUsbDeviceHS.pClassData;
+  if (hcdc->TxState != 0){
+    return USBD_BUSY;
+  }
+  USBD_CDC_SetTxBuffer(&hUsbDeviceHS, Buf, Len);
+  result = USBD_CDC_TransmitPacket(&hUsbDeviceHS);
+#else
+  //Check length
+  if (Len > CDC_DATA_HS_MAX_PACKET_SIZE)
+      return USBD_FAIL;
+
+  // Check for ongoing transmission
+  USBD_CDC_HandleTypeDef *hcdc = (USBD_CDC_HandleTypeDef*)hUsbDeviceHS.pClassData;
+  if(hcdc->TxState != 0)
+      return USBD_BUSY;
+
+  // memcpy Buf into UserTxBufferHS
+  memcpy(UserTxBufferHS, Buf, Len);
+
+  // Update Len
+  USBD_CDC_SetTxBuffer(&hUsbDeviceHS, UserTxBufferHS, Len);
+  result = USBD_CDC_TransmitPacket(&hUsbDeviceHS);
+#endif
+  /* USER CODE END 12 */
+  return result;
+}
+
+/**
+  * @brief  CDC_TransmitCplt_HS
+  *         Data transmitted callback
+  *
+  *         @note
+  *         This function is IN transfer complete callback used to inform user that
+  *         the submitted Data is successfully sent over USB.
+  *
+  * @param  Buf: Buffer of data to be received
+  * @param  Len: Number of data received (in bytes)
+  * @retval Result of the operation: USBD_OK if all operations are OK else USBD_FAIL
+  */
+static int8_t CDC_TransmitCplt_HS(uint8_t *Buf, uint32_t *Len, uint8_t epnum)
+{
+  uint8_t result = USBD_OK;
+  /* USER CODE BEGIN 14 */
+  UNUSED(Buf);
+  UNUSED(Len);
+  UNUSED(epnum);
+  /* USER CODE END 14 */
+  return result;
+}
+
+/* USER CODE BEGIN PRIVATE_FUNCTIONS_IMPLEMENTATION */
+// USB ULPI的读写操作实现
+// 硬汉论坛：https://forum.anfulai.cn/forum.php?mod=viewthread&tid=127602&highlight=USB%2BULPI%B5%C4%B6%C1%D0%B4%B2%D9%D7%F7%CA%B5%CF%D6
+// 【使用】ST论坛H750+USB3300：https://community.st.com/t5/stm32-mcus-embedded-software/stm32h750-usb3300-no-signals-on-ulpi/td-p/225293
+
+#define USBULPI_PHYCR     ((uint32_t)(0x40040000 + 0x034))
+#define USBULPI_D07       ((uint32_t)0x000000FF)
+#define USBULPI_New       ((uint32_t)0x02000000)
+#define USBULPI_RW        ((uint32_t)0x00400000)
+#define USBULPI_S_BUSY    ((uint32_t)0x04000000)
+#define USBULPI_S_DONE    ((uint32_t)0x08000000)
+
+#define USB_OTG_READ_REG32(reg)  (*(__IO uint32_t *)(reg))
+#define USB_OTG_WRITE_REG32(reg,value) (*(__IO uint32_t *)(reg) = (value))
+
+uint32_t USB_ULPI_Read(uint32_t Addr)
+{
+    __IO uint32_t val = 0;
+    __IO uint32_t timeout = 1000; /* Can be tuned based on the Clock or etc... */
+    __IO uint32_t bussy = 0;
+    USB_OTG_WRITE_REG32(USBULPI_PHYCR, USBULPI_New | (Addr << 16));
+    val = USB_OTG_READ_REG32(USBULPI_PHYCR);
+    while (((val & USBULPI_S_DONE) == 0) && (timeout--))
+    {
+        val = USB_OTG_READ_REG32(USBULPI_PHYCR);
+        bussy = val & USBULPI_S_BUSY;
+    }
+    val = USB_OTG_READ_REG32(USBULPI_PHYCR);
+    return  (val & 0x000000ff);
+}
+
+/**
+  * @brief  Write CR value
+  * @param  Addr the Address of the ULPI Register
+  * @param  Data Data to write
+  * @retval Returns value of PHY CR register
+  */
+uint32_t USB_ULPI_Write(uint32_t Addr, uint32_t Data)   /* Parameter is the Address of the ULPI Register & Date to write */
+{
+    __IO uint32_t val;
+    __IO uint32_t timeout = 100;   /* Can be tuned based on the Clock or etc... */
+    __IO uint32_t bussy = 0;
+
+    USB_OTG_WRITE_REG32(USBULPI_PHYCR, USBULPI_New | USBULPI_RW | (Addr << 16) | (Data & 0x000000ff));
+    val = USB_OTG_READ_REG32(USBULPI_PHYCR);
+    while (((val & USBULPI_S_DONE) == 0) && (timeout--))
+    {
+        val = USB_OTG_READ_REG32(USBULPI_PHYCR);
+        bussy = val & USBULPI_S_BUSY;
+    }
+
+    val = USB_OTG_READ_REG32(USBULPI_PHYCR);
+    return 0;
+}
+
+/**
+  * @brief  Customize the disconnect function before USB initialization to reduce the trouble of reset
+  * @notice Called within the "USER CODE BEGIN SysInit" section of main()
+  */
+void USB_Status_Init(void)
+{
+/*
+《USB3300 Hardware Design Checklist》.PDF
+    ## 7.1 复位电路
+    RESET 引脚是一个高电平有效的收发器复位。 RESET 引脚的使用是可选的。
+    RESET 引脚上的逻辑高电平与写入功能控制寄存器的复位（地址 04h，bit5）相同。
+    这不会重置 ULPI 寄存器组。 该引脚包括一个集成的接地下拉电阻。
+    如果不使用，该引脚可以悬空，但建议将该引脚接地。
+    如果 RESET 从外部源驱动为高电平，则逻辑高电平必须持续至少一个 CLKOUT 信号的完整周期。
+    在 RESET 信号取反后的两个 CLKOUT 时钟周期内，没有其他 PHY 数字输入信号可以改变状态。
+
+    《https://ww1.microchip.com/downloads/en/DeviceDoc/00001783C.pdf》.PDF P21
+    Field Name: Reset
+    Bit:5
+    Default: 0b
+    Active high transceiver reset. This reset does not reset the ULPI
+    interface or register set. Automatically clears after reset is
+    complete.
+
+ */
+    uint32_t reg_val;  // 存储04h寄存器当前值
+
+
+    // 步骤1：读取功能控制寄存器（04h）的当前值
+    reg_val = USB_ULPI_Read(0x04);
+
+    // 步骤2：置位bit5（触发复位，高电平有效）
+    USB_ULPI_Write(0x04, reg_val | (1 << 5));
+
+    HAL_Delay(2000);    // If it doesn't work, just extend the delay
+}
+/* USER CODE END PRIVATE_FUNCTIONS_IMPLEMENTATION */
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
